cb06c8bf3ba96faf61f2f88d1b1577fd5a553b88
izzy2fancy <121840901+izzy2fancy@users.noreply.github.com>
2023-12-16 19:20:44 -0500

    Update apply_patch.sh

--- tools/apply_patch.sh
+++ tools/apply_patch.sh
@@ -3,13 +3,6 @@
 # apply_patch.sh - Applies an enhancement patch
 #
 
-if [ "$#" -ne 1 ]
-then
-    echo "Usage: $0 patch_file"
-    echo '    Applies a patch file to the current directory'
-    exit 1
-fi
-
 read -p "Do you wish to apply the patch '$1'? [Y/N] " response
 case "$response" in
     Y|y)
